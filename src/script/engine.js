9af302e698f325e730c7f926bedac55714f98375
Flavia Oliveira <flaviacoliv@gmail.com>
2023-12-30 09:14:59 -0300

    Revisão de Código

--- src/script/engine.js
+++ src/script/engine.js
@@ -2,7 +2,7 @@ const state = {
     score: {
         playerScore: 0,
         computerScore: 0,
-        scoreBox: document.getElementById("score-points"),
+        scoreBox: document.getElementById("score_points"),
     },
 
     cardSprites: {
